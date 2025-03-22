7f001d493b314be76f58b57a2b141774f9eec141
StephanyCanuto <stephany.canutos25@gmail.com>
2025-03-22 12:41:47 -0300

    feat: utiliando Scanner, convertendo tipos de forma automática, conversão por casting explicito e parsing de String

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 //importando scanner
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
-       //Instanciando classe
+//       Instanciando classe
         Scanner scanner = new Scanner(System.in);
 
 
@@ -28,8 +29,41 @@ public class Main {
         System.out.println("A soma dos números é:  "+ (n1+n2));
 
 
+        //Conversão de Tipos Automática
+        int numeroInt = 10;
+        double numeroDouble = numeroInt; // Promoção automática
+        System.out.println(numeroDouble); // Saída: 10.0
 
+        //Conversão de Tipos com Casting Explpicito
+        double numDouble = 123.456;
+        int numInt = (int) numDouble; // Casting explícito
+        System.out.println(numInt); // Saída: 123 (perda da parte decimal)
 
+        //Parsing de String
+        String numeroTexto = "42";
+        int nInt = Integer.parseInt(numeroTexto);
+        System.out.println(nInt); // Saída: 42
 
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        String inputInt = scanner1.nextLine();
+
+        try {
+            int nmrInt = Integer.parseInt(inputInt);
+            System.out.println("Número inteiro convertido: " + nmrInt);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao converter para inteiro: " + e.getMessage());
         }
+
+        System.out.print("Digite um número decimal: ");
+        String inputDouble = scanner1.nextLine();
+
+        try {
+            double nmrDouble = Double.parseDouble(inputDouble);
+            System.out.println("Número decimal convertido: " + nmrDouble);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao converter para decimal: " + e.getMessage());
+        }
+
+    }
 }
