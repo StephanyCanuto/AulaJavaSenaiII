f0910cd8277779c80493f045bfc31ff111f9a282
StephanyCanuto <stephany.canutos25@gmail.com>
2025-03-22 12:55:46 -0300

    feat: exercicios - Calculadora Simples - Testando Variável - Trabalhando com Valores Flutuantes

--- src/Main.java
+++ src/Main.java
@@ -65,5 +65,34 @@ public class Main {
             System.out.println("Erro ao converter para decimal: " + e.getMessage());
         }
 
+
+        //Calculadora simples:
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Soma: " + (numero1 + numero2));
+        System.out.println("Subtração: " + (numero1 - numero2));
+        System.out.println("Multiplicação: " + (numero1 * numero2));
+        System.out.println("Divisão: " + (numero1 / numero2));
+
+        //Testando estouro da variável
+        byte valorByte = 127;
+        System.out.println("Valor inicial: " + valorByte);
+        valorByte++;
+        System.out.println("Após adicionar 1: " + valorByte);
+        valorByte++;
+        System.out.println("Após adicionar mais 1: " + valorByte);
+
+        //Trabalhando com valores Flutuantes
+        float valorFloat = 10.5F;
+        double valorDouble = 20.5;
+
+        System.out.println("Valor float: " + valorFloat);
+        System.out.println("Valor double: " + valorDouble);
+        double resultado = valorFloat + valorDouble;
+        System.out.println("Resultado da soma: " + resultado);
     }
 }
